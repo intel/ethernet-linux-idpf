6d1b0d65ba9947625889d830d7588bfe0b5b63d7
Lukasz Czapnik <lukasz.czapnik@intel.com>
2025-10-06 13:19:16 +0200

    compat: add FEC histogram support
    added new arument (struct ethtool_fec_hist *hist) to ice_get_fec_stats().
    
    Signed-off-by: Lukasz Czapnik <lukasz.czapnik@intel.com>
    Change-type: ImplementationChange

--- idpf/src/kcompat-generator.sh
+++ idpf/src/kcompat-generator.sh
@@ -174,6 +174,7 @@ function gen-ethtool() {
 	unlgeth='include/uapi/linux/ethtool_netlink_generated.h'
 	gen HAVE_ETHTOOL_COALESCE_EXTACK if method get_coalesce of ethtool_ops matches 'struct kernel_ethtool_coalesce \\*' in "$eth"
 	gen HAVE_ETHTOOL_EXTENDED_RINGPARAMS if method get_ringparam of ethtool_ops matches 'struct kernel_ethtool_ringparam \\*' in "$eth"
+	gen HAVE_ETHTOOL_FEC_HIST if method get_fec_stats of ethtool_ops matches 'struct ethtool_fec_hist \\*' in "$eth"
 	gen HAVE_ETHTOOL_GET_FEC_STATS_OPS if struct ethtool_ops matches '\\*get_fec_stats' in "$eth"
 	gen HAVE_ETHTOOL_KEEE if struct ethtool_keee in "$eth"
 	gen HAVE_ETHTOOL_KERNEL_TS_INFO if struct kernel_ethtool_ts_info in "$eth"
