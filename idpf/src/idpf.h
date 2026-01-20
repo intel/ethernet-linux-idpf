5fa3f620044acf35c153559210e24dc15bb2a666
Joshua Hay <joshua.a.hay@intel.com>
2026-01-20 14:06:55 -0800

    idpf: implement remaining IDC RDMA core callbacks and handlers
    RDMA core callbacks and handlers").
    
    Implement the idpf_idc_request_reset and idpf_idc_rdma_vc_send_sync
    callbacks for the rdma core auxiliary driver to issue reset events to
    the idpf and send (synchronous) virtchnl messages to the control plane
    respectively.
    
    Implement and plumb the reset handler for the opposite flow as well,
    i.e. when the idpf is resetiing and needs to notify the rdma core
    auxiliary driver.
    
    Signed-off-by: Joshua Hay <joshua.a.hay@intel.com>
    Change-type: Other

--- idpf/src/idpf.h
+++ idpf/src/idpf.h
@@ -84,6 +84,9 @@ struct idpf_rss_data;
 #define iidc_rdma_reset_type iidc_reset_type
 #define iidc_rdma_event iidc_event
 
+#define iidc_rdma_event_type iidc_event_type
+#define IIDC_RDMA_EVENT_WARN_RESET IIDC_EVENT_WARN_RESET
+
 #include "iidc_rdma_idpf.h"
 
 #define GETMAXVAL(num_bits)		GENMASK((num_bits) - 1, 0)
@@ -1395,6 +1398,7 @@ int idpf_idc_init_aux_core_dev(struct idpf_adapter *adapter,
 			       enum iidc_function_type ftype);
 void idpf_idc_deinit_core_aux_device(struct iidc_rdma_core_dev_info *cdev_info);
 void idpf_idc_deinit_vport_aux_device(struct iidc_rdma_vport_dev_info *vdev_info);
+void idpf_idc_issue_reset_event(struct iidc_rdma_core_dev_info *cdev_info);
 #ifdef DEVLINK_ENABLED
 void idpf_vport_dealloc(struct idpf_vport *vport);
 #endif /* DEVLINK_ENABLED */
